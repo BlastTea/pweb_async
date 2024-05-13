3531577d956ffa30bc03af9698758bb113bb4bf0
BlastTea <arya.jbr.999@gmail.com>
2024-05-13 20:13:58 +0700

    feat: add read, create, update, delete

---
+++ api/read.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-Type: application/json');
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=pweb_async', 'root', '');
+} catch (\Exception $e) {
+    die($e->getMessage());
+}
+
+$query = 'SELECT * FROM tour_packages';
+$sql = $db->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+$sql->execute();
+
+$results = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode(['message' => 'Data berhasil diambil', 'data' => $results]);
--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+document.addEventListener('DOMContentLoaded', async function() {
+  try {
+
+    var request = await fetch('http://127.0.0.1/kuliah/PWEB/Pertemuan%2013/api/read.php');
+    
+    var response = await request.json();
+
+    displayData(response);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+function displayData(data) {
+  const container = document.getElementById('data-container');
+  container.innerHTML = ''; // Membersihkan konten yang ada sebelumnya
+
+  // Cek apakah data ada dan memiliki elemen
+  if (data && data.length > 0) {
+      data.forEach(item => {
+          const productDiv = document.createElement('div');
+          productDiv.className = 'product';
+
+          // Menambahkan judul nama paket
+          const name = document.createElement('h2');
+          name.textContent = item.name;
+          productDiv.appendChild(name);
+
+          // Menambahkan deskripsi
+          const description = document.createElement('p');
+          description.textContent = item.description;
+          productDiv.appendChild(description);
+
+          // Menambahkan harga
+          const price = document.createElement('p');
+          price.textContent = `Harga: Rp${item.price}`;
+          productDiv.appendChild(price);
+
+          // Tambahkan elemen ke container
+          container.appendChild(productDiv);
+      });
+  } else {
+      container.innerHTML = '<p>No products available.</p>';
+  }
+}
+
+
 document.querySelector(".jsFilter").addEventListener("click", function () {
   document.querySelector(".filter-menu").classList.toggle("active");
 });
@@ -19,6 +66,7 @@ document.querySelector(".list").addEventListener("click", function () {
 });
 
 var modeSwitch = document.querySelector('.mode-switch');
-modeSwitch.addEventListener('click', function () {                      document.documentElement.classList.toggle('light');
- modeSwitch.classList.toggle('active');
+modeSwitch.addEventListener('click', function () {
+  document.documentElement.classList.toggle('light');
+  modeSwitch.classList.toggle('active');
 });
